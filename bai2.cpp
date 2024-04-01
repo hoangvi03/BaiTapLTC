8a6a263530a8a994d703749507f967d9ba66c769
hoangvi03 <162998787+hoangvi03@users.noreply.github.com>
2024-04-01 08:22:30 +0700

    bai2.cpp

---
+++ bai2.cpp
@@ -0,0 +1,27 @@
+//Trần Hoàng Vĩ
+#include <stdio.h>
+
+int isSoChinhPhuong(int n)
+{
+    for(int i = 1; i*i<= n; i++)
+    {
+        if(i*i == n)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main(void){
+   int n = 50;
+   int count = 0;
+   for (int i = 0; i<n; i++)
+   {
+       if(isSoChinhPhuong(i) == 1)
+       {
+           count++;
+           printf("%d ", i);
+       }
+   }
+}
